c4ac339efa680e2f2ade9ba1c2f05156dbf4514d
corlinesherenharli <corline.sheren@student.pradita.ac.id>
2025-12-15 10:17:06 +0700

    index.html

--- palu/career/models.py
+++ palu/career/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 
 class Career(models.Model):
-    title = models.CharField(max_length=200)
-    company = models.CharField(max_length=200)
+    title = models.CharField(max_length=100)
+    company = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
     description = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
